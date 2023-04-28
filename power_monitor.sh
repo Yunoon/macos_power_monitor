4a85ce48bd4780d1d684034430b79a54aacb1645
Yunnon <yunoon233@gmail.com>
2023-04-28 14:19:44 +0800

    增加了log日志记录

--- power_monitor.sh
+++ power_monitor.sh
@@ -3,17 +3,17 @@
 while true; do
     # 获取当前电源状态
     power_status=$(pmset -g ps | grep "Now drawing from" | awk '{print $4}')
-    echo $power_status    
+    echo `date +%H:%M:%S`-$power_status >> /Users/yunoon/Scripts/logs/power_monitor-`date +%Y-%m-%d`-logs.txt
     # 如果当前电源状态为“AC Power”，禁止睡眠
     if [ "$power_status" = "'AC" ]; then
         sudo pmset -b sleep 0
         sudo pmset -b disablesleep 1
-	echo "AC On! Disablesleep!"
+	echo `date +%H:%M:%S`-"AC On! Disablesleep!" >> /Users/yunoon/Scripts/logs/power_monitor-`date +%Y-%m-%d`-logs.txt
     # 如果当前电源状态为“Battery Power”，允许睡眠
     elif [ "$power_status" = "'Battery" ]; then
         sudo pmset -b sleep 0
         sudo pmset -b disablesleep 0
-	echo "AC Off! Enablesleep!"
+	echo `date +%H:%M:%S`-"AC Off! Enablesleep!" >> /Users/yunoon/Scripts/logs/power_monitor-`date +%Y-%m-%d`-logs.txt
     fi
 
     # 每隔10秒检查一次电源状态
